fd62a1251be4d19e58fa4d4feb9f1b1d0fb72636
Techsarasumbul <techsarasumbul@gmail.com>
2023-11-05 19:27:42 +0530

    amazon using react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+
+function App(){
+    return(
+        <div className="App">
+             <Header />
+            <h1>We are buiding Amazon Project</h1>
+            
+           
+        </div>
+    );
+}
+export default App;
